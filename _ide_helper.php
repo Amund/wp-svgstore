1f77d8d87f888b6cce7ce21ac771552016012b2d
Amund <mailamund@gmail.com>
2025-04-17 23:11:51 +0200

    fix: dev tooling

---
+++ _ide_helper.php
@@ -0,0 +1,11 @@
+<?php
+
+define('WP_CLI', true);
+
+class WP_CLI
+{
+    static function add_command() {}
+    static function success() {}
+    static function warning() {}
+    static function error() {}
+}
